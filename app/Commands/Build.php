d12aa695285db295b5694940fbd421c0105f13b6
Daniel Pegg <dan@parall.ax>
2019-11-11 15:42:56 +0000

    Make modular

--- app/Commands/Build.php
+++ app/Commands/Build.php
@@ -21,8 +21,7 @@ class Build extends Command
                             {--AWS_ID= : AWS Access ID }
                             {--AWS_SECRET= : AWS Secret Access Key}
                             {--no-build= : If true, docker build won\'t run}
-                            {--docker-image= : Docker image to build project in}
-                            {--out-dir= : Folder built files are located}';
+                            {--out-dir= : Folder built files are placed}';
 
     /**
      * The description of the command.
@@ -41,110 +40,113 @@ class Build extends Command
         // Set environment variables
         if (isset($_ENV) && count($_ENV) > 1) {
             // Add deploy_environment variables from bamboo_env_ keys
-            $environmentVariables;
-
             foreach ($_ENV as $key => $value) {
-                if (substr($key, 0, 11 ) === "bamboo_docker_") {
-                    $dockerVariables[substr($key, 13)] = $value;
-                } elseif (substr($key, 0, 11 ) === "bamboo_") {
-                    $bambooVariables[substr($key, 7)] = $value;
+                if (substr($key, 0, 14 ) === "bamboo_docker_") {
+                    $dockerVariables[substr($key, 14)] = $value;
+                } elseif (substr($key, 0, 7 ) === "bamboo_") {
+                    $environmentVariables[substr($key, 7)] = $value;
                 }
             }
-            if (count($dockerVariables) > 0) {
+
+            if (isset($dockerVariables) && count($dockerVariables) > 0) {
                 // Cast each environment variable as a string
                 foreach ($dockerVariables as $key => $value) {
                     $dockerVariables[$key] = (string) $value;
                 }
             }
-            if (count($bambooVariables) > 0) {
+
+            if (count($environmentVariables) > 0) {
                 // Cast each environment variable as a string
-                foreach ($bambooVariables as $key => $value) {
-                    $bambooVariables[$key] = (string) $value;
+                foreach ($environmentVariables as $key => $value) {
+                    $environmentVariables[$key] = (string) $value;
                 }
             }
         }   
 
-        if (null == $this->option('app')) { echo 'Please set the --app option'; exit(1);}
-        if (null == $this->option('branch')) { echo 'Please set the --branch option'; exit(1);}
-        if (null == $this->option('environment')) { echo 'Please set the --environment option'; exit(1);}
-        if (null == $this->option('AWS_ID')) { echo 'Please set the --AWS_ID option'; exit(1);}
-        if (null == $this->option('AWS_SECRET')) { echo 'Please set the --AWS_SECRET option'; exit(1);}
-        if (null == $this->option('docker-image')) {
-            if (!isset($environmentVariables['docker-image'])) {
-                echo 'Using default image (node:10)';
-                $Image = 'node:10';
-            } else {
-                $Image = $environmentVariables['docker-image'];
-            }
-        } else {
-            $Image = $this->option('docker-image');
+        // Error on missing needed values
+        if (null == $this->option('app')) { echo 'Please set the --app option'; exit(1);} else {$environmentVariables['app'] = $this->option('app');}
+        if (null == $this->option('branch')) { echo 'Please set the --branch option'; exit(1);} else {$environmentVariables['branch'] = $this->option('branch');}
+        if (null == $this->option('environment')) { echo 'Please set the --environment option'; exit(1);} else {$environmentVariables['environment'] = $this->option('environment');}
+        if (!isset($environmentVariables['AWS_ID'])) { echo 'Please set the --AWS_ID option'; exit(1);}
+        if (!isset($environmentVariables['AWS_SECRET'])) { echo 'Please set the --AWS_SECRET option'; exit(1);}
+
+        // Set default values on optional values
+        if (!isset($environmentVariables['IMAGE'])) {
+            $environmentVariables['IMAGE'] = 'node:10';
         }
-        if (null == $this->option('out-dir')) {
-            if (!isset($environmentVariables['out-dir'])) {
-                echo 'Using default out directory (dist)';
-                $outDirectory = 'dist';
-            } else {
-                $outDirectory = $environmentVariables['out-dir'];
-            }
-        } else {
-            $outDirectory = $this->option('out-dir');
+        if (!isset($environmentVariables['OUT_DIR'])) {
+            $environmentVariables['OUT_DIR'] = 'dist';
+        }
+        if (!isset($environmentVariables['INDEX_FILE'])) {
+            $environmentVariables['INDEX_FILE'] = 'index.html';
+        }
+        if (!isset($environmentVariables['ERROR_FILE'])) {
+            $environmentVariables['ERROR_FILE'] = 'error.html';
+        }
+        if (!isset($environmentVariables['BUCKET_NAME'])) {
+            $environmentVariables['BUCKET_NAME'] = 's3build-' . $environmentVariables['app'] . '-' . $environmentVariables['branch'] . '-' . $environmentVariables['environment'];
+        }
+        if (!isset($environmentVariables['COMMAND'])) {
+            $environmentVariables['COMMAND'] = 'make';
         }
 
         // Set S3 bucket Variables
-        $Bucket = 's3build-' . $this->option('app') . '-' . $this->option('branch') . '-' . $this->option('environment');
         $Policy = file_get_contents(__DIR__.'/../../config/PublicBucket.json');
-        $Policy = str_replace('{{ bucket }}', $Bucket, $Policy);
+        $Policy = str_replace('{{ bucket }}', $environmentVariables['BUCKET_NAME'], $Policy);
 
         // Define S3 Client
         $s3Client = new S3Client([
             'region' => 'eu-west-1',
             'version' => '2006-03-01',
             'credentials' => [
-                'key'    => $this->option('AWS_ID'),
-                'secret' => $this->option('AWS_SECRET'),
+                'key'    => $environmentVariables['AWS_ID'],
+                'secret' => $environmentVariables['AWS_SECRET'],
             ],
         ]);
-
-        if (null == $s3Client->headBucket( array('Bucket' => $Bucket))) {
+ini_set('memory_limit', '-1');
+        //if (null == $s3Client->headBucket( array('Bucket' => $environmentVariables['BUCKET_NAME']))) {
             // Create S3 bucket
             try {
                 $result = $s3Client->createBucket([
-                    'Bucket' => $Bucket,
+                    'Bucket' => $environmentVariables['BUCKET_NAME'],
                 ]);
             } catch (AwsException $e) {
                 echo $e->getMessage();
-                echo "\n";
             }
         //}
 
         if ($this->option('no-build') !== 'true' ) {
-            if (isset($environmentVariables)) {
+            if (isset($dockerVariables)) {
     
                 $env = array();
+
                 foreach ($dockerVariables as $key => $value) {
                     array_push($env, $key.'='.$value);
                 }
 
-                $command = 'docker run --volume ' . getcwd() . ':/data --workdir /data --rm -e ' . implode(' -e ', $env) . ' ' . $Image . ' make';
-                shell_exec($command);
+                $command = 'docker run --volume ' . getcwd() . ':/data --workdir /data --rm -e ' . implode(' -e ', $env) . ' ' . $environmentVariables['IMAGE'] . ' ' . $environmentVariables['COMMAND'];
+                print_r($command);
+                exit(0);
     
             } else {
     
-                $command = 'docker run --volume ' . getcwd() . ':/data --workdir /data --rm ' . $Image . ' make';
-                shell_exec($command);
+                $command = 'docker run --volume ' . getcwd() . ':/data --workdir /data --rm ' . $environmentVariables['IMAGE'] . ' ' . $environmentVariables['COMMAND'];
+                print_r($command);
+                exit(0);
             }
         }
+
         // Upload site
-        $s3Client->uploadDirectory($outDirectory.'/', $Bucket);
+        $s3Client->uploadDirectory($environmentVariables['OUT_DIR'].'/', $environmentVariables['BUCKET_NAME']);
 
         $result = $s3Client->putBucketWebsite([
-            'Bucket' => $Bucket,
+            'Bucket' => $environmentVariables['BUCKET_NAME'],
             'WebsiteConfiguration' => [
                 'ErrorDocument' => [
-                    'Key' => '200.html',
+                    'Key' => $environmentVariables['ERROR_FILE'],
                 ],
                 'IndexDocument' => [
-                    'Suffix' => 'index.html',
+                    'Suffix' => $environmentVariables['INDEX_FILE'],
                 ],
             ],
         ]);
@@ -152,10 +154,10 @@ class Build extends Command
         // Sets public bucket policy
         try {
             $resp = $s3Client->putBucketPolicy([
-                'Bucket' => $Bucket,
+                'Bucket' => $environmentVariables['BUCKET_NAME'],
                 'Policy' => $Policy,
             ]);
-            echo "Succeed in put a policy on bucket: " . $Bucket . "\n";
+            echo "Succeed in put a policy on bucket: " . $environmentVariables['BUCKET_NAME'] . "\n";
         } catch (AwsException $e) {
             // Display error message
             echo $e->getMessage();
